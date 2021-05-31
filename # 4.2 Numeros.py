159d1dbdb19a582439c19b0109cea97f1933b7a8
Luiza Mercês <84783323+luizamerces@users.noreply.github.com>
2021-05-31 11:50:49 -0300

    Add files via upload

---
+++ # 4.2 Numeros.py
@@ -0,0 +1,6 @@
+# 4.2 Numeros 
+x = int(input("Escreva um valor inteiro:"))
+y = int(input("Escreva outro valor inteiro:"))
+
+z = (x**2 + y**2)/ (x-y)**2
+print("O resultado e ", int(z))
---
+++ #Exercicio numeros 2.py
@@ -0,0 +1,4 @@
+#Exercicio numeros 2#
+sal = float(input("Escreva seu salario: "))
+aj = 1.35*sal
+print("Novo salario com reajuste: R$", aj)
